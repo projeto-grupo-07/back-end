d6463071a16daa874736d6d0e5eeb0133512a8f7
GuilhermeAugustoFigueiredo <gui.figueiredo136@gmail.com>
2026-05-14 17:34:32 -0300

    fix: correfação de chamad e lgos emitir relatorio

--- src/main/java/school/sptech/crud_proj_v1/controller/RelatorioController.java
+++ src/main/java/school/sptech/crud_proj_v1/controller/RelatorioController.java
@@ -2,11 +2,14 @@ package school.sptech.crud_proj_v1.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import school.sptech.crud_proj_v1.dto.Relatorio.RelatorioRequest;
 import school.sptech.crud_proj_v1.relatorio.RabbitImportProducer;
 import school.sptech.crud_proj_v1.dto.RabbitMQ.JobResponse;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/relatorio")
@@ -26,7 +29,7 @@ public class RelatorioController {
     public ResponseEntity<JobResponse> enfileirarRelatorio(@PathVariable String fileKey) {
 
         String normalizedKey = fileKey.startsWith("/") ? fileKey.substring(1) : fileKey;
-        log.info("Recebendo requisição de importação. FileKey: {}", normalizedKey);
+        log.info("Recebendo requisição de gerar relatorio de importação. FileKey: {}", normalizedKey);
         try {
             String jobId = producer.publish(normalizedKey);
             log.info("Importação enfileirada com sucesso. JobId: {}, FileKey: {}", jobId, normalizedKey);
@@ -38,16 +41,21 @@ public class RelatorioController {
     }
 
     @PostMapping("/gerar")
-    public ResponseEntity<String> solicitarRelatorioNaLambdaAWS(@RequestParam Integer mes, @RequestParam Integer ano) {
-        String url = String.format("%s/relatorio?ano=%d&mes=%d", apiGatewayURL, ano, mes);
-        log.info("Enviando requisição para API externa. URL: {}", url);
+    public ResponseEntity<String> solicitarRelatorioNaLambdaAWS(@RequestBody RelatorioRequest req) {
+        Integer mes = req.mes();
+        Integer ano = req.ano();
+        String url = String.format("%s/solicitar-relatorio", apiGatewayURL);
+        log.info("Requisição para emitir relatorio de ano: {} e mes: {}. Utilizado a url: {}", ano, mes, url);
         try {
-            ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-            log.info("Resposta recebida da API externa. Status: {}, Body: {}", response.getStatusCode(), response.getBody());
-            return response;
+            Map<String,Object> payload = Map.of("ano", ano, "mes", mes);
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            HttpEntity<Map<String,Object>> request = new HttpEntity<>(payload, headers);
+            ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
+            return ResponseEntity.status(response.getStatusCode()).body(response.getBody());
         } catch (Exception e) {
-            log.error("Erro ao chamar API externa para emissão de relatório. URL: {}", url, e);
-            throw new RuntimeException("Falha ao emitir relatório", e);
+            log.error("Erro ao chamar API externa", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Falha ao emitir relatório");
         }
     }
 }
---
+++ src/main/java/school/sptech/crud_proj_v1/dto/Relatorio/RelatorioRequest.java
@@ -0,0 +1,6 @@
+package school.sptech.crud_proj_v1.dto.Relatorio;
+
+public record RelatorioRequest(
+        Integer mes,
+        Integer ano
+) {}
